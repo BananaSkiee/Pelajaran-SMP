b8ac445563bbf3103ca19a377b3b4d08698756b7
MinimalMikir <bananaskiee@gmail.com>
2025-08-07 22:47:03 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,37 @@
+const scheduleData = {
+    '9.2': {
+        'Senin': [
+            { time: '07.00 - 07.35', subject: 'Upacara' },
+            { time: '07.35 - 09.20', subject: 'Bahasa Inggris', teacher: 'Indah Masruroh, S.Pd.' },
+            { time: '09.35 - 10.45', subject: 'TIK & PJOK', teacher: 'Oktaftianus Gea' },
+            { time: '10.45 - 11.20', subject: 'TIK & PJOK', teacher: 'Oktaftianus Gea' },
+            { time: '11.20 - 12.30', subject: 'BK', teacher: 'Ummu Syarifah, S.Pd.' }
+        ],
+        'Selasa': [
+            { time: '07.00 - 08.45', subject: 'Bahasa Indonesia', teacher: 'Ali Susanto, M.MPd' },
+            { time: '08.45 - 09.20', subject: 'Bahasa Inggris', teacher: 'Indah Masruroh, S.Pd.' },
+            { time: '09.35 - 11.20', subject: 'PPSC', teacher: 'D.Sumadinata, S.Pd.' },
+            { time: '11.20 - 12.30', subject: 'Matematika', teacher: 'Indah Masruroh, S.Pd.' }
+        ],
+        'Rabu': [
+            { time: '07.00 - 08.45', subject: 'Bahasa Indonesia', teacher: 'Ali Susanto, M.MPd' },
+            { time: '08.45 - 10.10', subject: 'Matematika', teacher: 'Indah Masruroh, S.Pd.' },
+            { time: '10.10 - 11.20', subject: 'PPSC', teacher: 'D.Sumadinata, S.Pd.' },
+            { time: '11.20 - 12.30', subject: 'Bahasa Inggris', teacher: 'Indah Masruroh, S.Pd.' }
+        ],
+        'Kamis': [
+            { time: '07.00 - 08.45', subject: 'IPA', teacher: 'Heri Suryanto, S.Pd.' },
+            { time: '08.45 - 09.20', subject: 'IPS & PPSC', teacher: 'Heris Mujiati, S.Pd.' },
+            { time: '09.35 - 10.10', subject: 'IPA', teacher: 'Heri Suryanto, S.Pd.' },
+            { time: '10.10 - 11.20', subject: 'Bahasa Inggris', teacher: 'Indah Masruroh, S.Pd.' },
+            { time: '11.20 - 12.30', subject: 'IPA', teacher: 'Heri Suryanto, S.Pd.' }
+        ],
+        'Jumat': [
+            { time: '07.00 - 07.35', subject: 'Pramuka', teacher: 'D.Sumadinata, S.Pd.' },
+            { time: '07.35 - 09.20', subject: 'IPS & PPSC', teacher: 'Heris Mujiati, S.Pd.' },
+            { time: '09.35 - 10.45', subject: 'IPS & PPSC', teacher: 'Heris Mujiati, S.Pd.' },
+            { time: '10.45 - 11.20', subject: 'IPA', teacher: 'Heri Suryanto, S.Pd.' },
+            { time: '11.20 - 12.30', subject: 'Bahasa Indonesia', teacher: 'Ali Susanto, M.MPd' }
+        ]
+    }
+};
