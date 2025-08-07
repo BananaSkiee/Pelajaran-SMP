6c56698833cc467f65d95357363427f86b2eb0da
MinimalMikir <bananaskiee@gmail.com>
2025-08-07 22:58:56 +0700

    Update data.js

--- data.js
+++ data.js
@@ -176,27 +176,27 @@ const scheduleData = {
     '9.2': {
         'Senin': [
             { time: '07.00 - 07.35', code: '', subject: 'Upacara', teacher: '' },
-            { time: '07.35 - 09.20', code: '17', subject: 'Bahasa Inggris', teacher: 'Indah Masruroh, S.Pd.' },
+            { time: '07.35 - 09.20', code: '10', subject: 'Bahasa Inggris', teacher: 'Indah Masruroh, S.Pd.' },
             { time: '09.35 - 11.20', code: '15', subject: 'TIK & PJOK', teacher: 'Oktaftianus Gea' },
-            { time: '11.20 - 12.30', code: '16', subject: 'BK', teacher: 'Ummu Syarifah, S.Pd.' }
+            { time: '11.20 - 12.30', code: '12', subject: 'BK', teacher: 'Ummu Syarifah, S.Pd.' }
         ],
         'Selasa': [
-            { time: '07.00 - 08.45', code: '1', subject: 'Bahasa Indonesia', teacher: 'Ali Susanto, M.MPd' },
-            { time: '08.45 - 09.20', code: '17', subject: 'Bahasa Inggris', teacher: 'Indah Masruroh, S.Pd.' },
+            { time: '07.00 - 08.45', code: '13', subject: 'Bahasa Indonesia', teacher: 'Ali Susanto, M.MPd' },
+            { time: '08.45 - 09.20', code: '10', subject: 'Bahasa Inggris', teacher: 'Indah Masruroh, S.Pd.' },
             { time: '09.35 - 11.20', code: '3', subject: 'PPSC', teacher: 'D.Sumadinata, S.Pd.' },
-            { time: '11.20 - 12.30', code: '17', subject: 'Matematika', teacher: 'Indah Masruroh, S.Pd.' }
+            { time: '11.20 - 12.30', code: '5', subject: 'Matematika', teacher: 'Indah Masruroh, S.Pd.' }
         ],
         'Rabu': [
-            { time: '07.00 - 08.45', code: '1', subject: 'Bahasa Indonesia', teacher: 'Ali Susanto, M.MPd' },
-            { time: '08.45 - 10.10', code: '17', subject: 'Matematika', teacher: 'Indah Masruroh, S.Pd.' },
+            { time: '07.00 - 08.45', code: '13', subject: 'Bahasa Indonesia', teacher: 'Ali Susanto, M.MPd' },
+            { time: '08.45 - 10.10', code: '5', subject: 'Matematika', teacher: 'Indah Masruroh, S.Pd.' },
             { time: '10.10 - 11.20', code: '3', subject: 'PPSC', teacher: 'D.Sumadinata, S.Pd.' },
-            { time: '11.20 - 12.30', code: '17', subject: 'Bahasa Inggris', teacher: 'Indah Masruroh, S.Pd.' }
+            { time: '11.20 - 12.30', code: '10', subject: 'Bahasa Inggris', teacher: 'Indah Masruroh, S.Pd.' }
         ],
         'Kamis': [
             { time: '07.00 - 08.45', code: '11', subject: 'IPA', teacher: 'Heri Suryanto, S.Pd.' },
             { time: '08.45 - 09.20', code: '2', subject: 'IPS & PPSC', teacher: 'Heris Mujiati, S.Pd.' },
             { time: '09.35 - 10.10', code: '11', subject: 'IPA', teacher: 'Heri Suryanto, S.Pd.' },
-            { time: '10.10 - 11.20', code: '17', subject: 'Bahasa Inggris', teacher: 'Indah Masruroh, S.Pd.' },
+            { time: '10.10 - 11.20', code: '10', subject: 'Bahasa Inggris', teacher: 'Indah Masruroh, S.Pd.' },
             { time: '11.20 - 12.30', code: '11', subject: 'IPA', teacher: 'Heri Suryanto, S.Pd.' }
         ],
         'Jumat': [
@@ -204,8 +204,7 @@ const scheduleData = {
             { time: '07.35 - 09.20', code: '2', subject: 'IPS & PPSC', teacher: 'Heris Mujiati, S.Pd.' },
             { time: '09.35 - 10.45', code: '2', subject: 'IPS & PPSC', teacher: 'Heris Mujiati, S.Pd.' },
             { time: '10.45 - 11.20', code: '11', subject: 'IPA', teacher: 'Heri Suryanto, S.Pd.' },
-            { time: '11.20 - 12.30', code: '1', subject: 'Bahasa Indonesia', teacher: 'Ali Susanto, M.MPd' }
+            { time: '11.20 - 12.30', code: '13', subject: 'Bahasa Indonesia', teacher: 'Ali Susanto, M.MPd' }
         ]
     }
 };
-             
